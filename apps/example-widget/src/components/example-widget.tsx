2f6b74052616dabc56259db5dd6f34aab862f683
andrewtookay <andrewtookay@gmail.com>
2024-04-03 14:24:44 +0300

    add total user rating computation

--- apps/example-widget/src/components/example-widget.tsx
+++ apps/example-widget/src/components/example-widget.tsx
@@ -5,40 +5,76 @@ import Text from '@akashaorg/design-system-core/lib/components/Text';
 import { useGetBeamsByAuthorDidQuery } from '@akashaorg/ui-awf-hooks/lib/generated/apollo';
 import { useGetLogin } from '@akashaorg/ui-awf-hooks';
 import axios from 'axios';
+import getSDK from "@akashaorg/awf-sdk";
 
 const ExampleWidget: React.FC<RootComponentProps> = () => {
   const loginData = useGetLogin();
   const ethereumAddress = loginData?.data?.ethAddress;
   const authenticatedDID = loginData?.data?.id;
   const [ipfsHashes, setIpfsHashes] = useState([]);
+  const [userRating, setUserRating] = useState(0);
   const [canMint, setCanMint] = useState(false);
+  const sdk = getSDK();
 
   const { data, loading, error } = useGetBeamsByAuthorDidQuery({
     variables: { id: authenticatedDID, first: 100 },
   });
 
-  const ownRating = React.useMemo(() => {
-    if (data && data.node && "akashaBeamList" in data.node) {
-      let ownRating = 0;
+  const beams = React.useMemo(() => {
+    return data && data.node && "akashaBeamList" in data.node ? data.node.akashaBeamList.edges : [];
+  }, [data]);
 
-      for (const beam of data.node.akashaBeamList.edges) {
-        if (beam.node.aiRating) {
-          ownRating += beam.node.aiRating;
-        }
-      }
-
-      return ownRating;
-    } else {
+  const computeUserRating = async () => {
+    const beamIds = beams.map(beam => beam.node.id);
+    console.log("beamIds:", beamIds);
+    
+    if (beamIds.length == 0) {
       return 0;
     }
-  }, [data]);
+
+    const response = await sdk.services.gql.client.GetUserRatings({ first: 100, filters: { where: { beamID: { in: beamIds }}}});
+    console.log("userRatings: ", response.userRatingIndex.edges);
+
+    const ratingsByBeamId = response.userRatingIndex.edges.reduce((acc, rating) => {
+      if (!acc[rating.node.beamID]) {
+         acc[rating.node.beamID] = [];
+      }
+      acc[rating.node.beamID].push(rating.node);
+      return acc;
+    }, {});
+    console.log("ratingsByBeamId:", ratingsByBeamId);
+
+    let sumOfAverages = 0;
+    for (const beamId in ratingsByBeamId) {
+      const ratings = ratingsByBeamId[beamId];
+      const totalRating = ratings.reduce((sum, rating) => sum + rating.userRating, 0);
+      const averageRating = parseInt((totalRating / ratings.length).toString());
+      sumOfAverages += averageRating;
+    }
+
+    console.log("total user rating: ", sumOfAverages);
+    setUserRating(sumOfAverages);
+  };
+
+  const aiRating = React.useMemo(() => {
+    let aiRating = 0;
+
+    for (const beam of beams) {
+      if (beam.node.aiRating) {
+        aiRating += beam.node.aiRating;
+      }
+    }
+
+    console.log("total AI rating: ", aiRating);
+    return aiRating;
+  }, [beams]);
 
   const handleUpdateMintableNfts = async () => {
     try {
-      console.log(ownRating);
+      console.log(aiRating);
       const response = await axios.post(`${process.env.API_ENDPOINT}/api/update-mintable-nfts`, {
         address: ethereumAddress,
-        rating: ownRating
+        rating: aiRating
       });
       console.log(response.data);
     } catch (error) {
@@ -93,14 +129,20 @@ const ExampleWidget: React.FC<RootComponentProps> = () => {
     }
   }, [ethereumAddress]);
 
+  useEffect(() => {
+    computeUserRating();
+  }, [beams]);
+
   return (
     <Card customStyle="flex place-self-center">
       <Text align="center">🔨🔥 GM ETH Bucharest! This is the Bubble Breaker! 🔥🔨</Text>
       <div className='flex-column'>
+        <div className="nft-separator">
+          <Text align="center">Total rating: {userRating + aiRating}</Text>
+        </div>
         <div className="nft-separator">NFT Actions</div>
         <button className='nft-button' onClick={handleUpdateMintableNfts}>Update Mintable NFTs</button>
         <button className='nft-button' onClick={checkMintableNfts}>Check Mintable NFTs</button>
-
         { canMint && <button className='nft-button' onClick={handleMintNfts}>Mint NFTs</button> }
         <div className="nft-separator">Your NFTs</div>
         <div className='nft-row'>
